7c9a1036f78b6820fe3eca319c0f397ceab0c4c0
Jakub Sokołowski <jakub@status.im>
2018-10-16 16:51:10 -0400

    add a nodemon task to gulp

--- gulpfile.js
+++ gulpfile.js
@@ -1,9 +1,18 @@
 const gulp = require('gulp')
 const babel = require('gulp-babel')
 const concat = require('gulp-concat-util')
-const count = require('gulp-count')
 const clean = require('gulp-clean')
 const debug = require('gulp-debug');
+const nodemon = require('gulp-nodemon');
+
+gulp.task('devel', () => {
+  nodemon({
+      script: 'src/index.js',
+      presets: ['env', 'stage-2'],
+      exec: 'babel-node'
+    })
+    .on('restart', () => { console.log('>> node restart') })
+});
 
 gulp.task('clean', () =>
   gulp.src('dist/*').pipe(clean())
@@ -11,9 +20,8 @@ gulp.task('clean', () =>
 
 gulp.task('build', () =>
   gulp.src('src/**/*.js')
-    .pipe(count('## js-files selected'))
+    //.pipe(debug({title: 'debug:'}))
     .pipe(babel())
-    .pipe(debug({title: 'debug:'}))
     .pipe(concat.scripts('counter.js'))
     .pipe(gulp.dest('dist/'))
 )
