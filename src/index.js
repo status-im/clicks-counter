13ca85c1c7c158d530c486ac79358003de321427
Jakub Sokołowski <jakub@status.im>
2018-10-16 13:25:04 -0400

    add basic koa app

--- src/index.js
+++ src/index.js
@@ -1,1 +1,12 @@
-console.log('Hello Node.js project.')
+const Koa = require('koa');
+const app = new Koa();
+
+
+let LISTEN_PORT = 3000
+
+app.use(async ctx => {
+    ctx.body = 'Hello World';
+});
+
+app.listen(LISTEN_PORT);
+console.log(`Started at: http://localhost:${LISTEN_PORT}/`)
