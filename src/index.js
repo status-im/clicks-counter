b38edbaa2604e5346b322064a4b7cd8387a45cb0
Jakub Sokołowski <jakub@status.im>
2018-10-16 16:09:41 -0400

    use env variables for config
    Signed-off-by: Jakub Sokołowski <jakub@status.im>

--- src/index.js
+++ src/index.js
@@ -6,9 +6,9 @@ import Counter from './counter'
 import Redis from 'async-redis'
 
 /* DEFAULTS */
-const REDIS_HOST = 'localhost'
-const REDIS_PORT = 6379
-const LISTEN_PORT = 3000
+const REDIS_HOST = process.env.REDIS_HOST || 'localhost'
+const REDIS_PORT = process.env.REDIS_PORT || 6379
+const LISTEN_PORT = process.env.LISTEN_PORT || 3000
 
 const app = new Koa()
 const router = new Router()
@@ -29,4 +29,5 @@ app.use(JSON({pretty: true}))
    .use(router.allowedMethods())
 
 app.listen(LISTEN_PORT)
+console.log(`Redis connection: ${REDIS_HOST}:${REDIS_PORT}`)
 console.log(`Started at: http://localhost:${LISTEN_PORT}/`)
